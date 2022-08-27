3cbbcfd674888e21f4e3653909fe1d84568ba0bd
Erlan Abdullaev <abdullaev.erlan.kk@gmail.com>
2022-08-27 15:27:50 +0600

    add change files

---
+++ js.js
@@ -0,0 +1,29 @@
+// let yourNumber = prompt("Введите число");
+// yourNumber--;
+// document.write(yourNumber);
+
+// let hour = 00;
+// if (hour >= 5 && hour <= 10) {
+//   console.log("Kutman tan");
+// } else if (hour > 10 && hour <= 18) {
+//   console.log("Kuyman kun");
+// } else if (hour > 18 && hour <= 22) {
+//   console.log("kutman kech");
+// } else {
+//   console.log("kutman tun");
+// }
+
+// let user = "Azgamat";
+// if (user == "Azamat") {
+//   console.log("Hello Azamat");
+// } else {
+//   console.log("hello user");
+// }
+
+let user = "";
+
+if (user) {
+  console.log("Hello " + user);
+} else {
+  console.log("I don't who you are!");
+}
