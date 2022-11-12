fed90fe4612cc534adc42f9756bdeb0348fbe925
Erlan Abdullaev <abdullaev.erlan.kk@gmail.com>
2022-11-12 23:54:50 +0600

    loan calc

--- calc/js/script.js
+++ calc/js/script.js
@@ -1,13 +1,24 @@
 const form = document.querySelector("#loan-form");
 
-form.addEventListener("submit", function (e) {
-  document.querySelector("#output").style.display = "none";
-  document.querySelector("#loader").style.display = "block";
+form.addEventListener("submit", calculateResults);
 
-  setTimeout(calculateResults, 2000);
+const output = document.querySelector("#output");
+output.style.display = "none";
+const loader = document.querySelector("#loader");
+loader.style.display = "none";
+loader.style.position = "relative";
 
-  e.preventDefault();
-});
+const img = document.querySelector("img");
+img.style.textAlign = "center";
+img.style.width = "160px";
+img.style.height = "200px";
+
+const err = document.querySelector(".error");
+err.style.display = "none";
+err.style.backgroundColor = "blueviolet";
+err.style.height = "40px";
+err.style.textAlign = "center";
+err.style.fontSize = "20px";
 
 function calculateResults(e) {
   const amount = document.querySelector("#amount");
@@ -29,26 +40,19 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
-    document.querySelector("#output").style.display = "block";
-    document.querySelector("#loader").style.display = "none";
+    loader.style.display = "block";
+    err.style.display = "none";
+    output.style.display = "none";
+    function setTime() {
+      loader.style.display = "none";
+      output.style.display = "block";
+      err.style.display = "none";
+    }
+    setTimeout(setTime, 1500);
   } else {
-    showError("Заполните все поля!");
+    err.style.display = "block";
+    output.style.display = "none";
   }
-}
-function showError(error) {
-  ocument.querySelector("#output").style.display = "none";
-  document.querySelector("#loader").style.display = "none";
 
-  const div = document.createComment("div");
-  const cardBoy = document.querySelector(".card-body");
-  const heading = document.querySelector(".heading");
-
-  div.className = "alert alert-danger";
-  div.appendChild(document.createTextNode(error));
-  cardBoy.insertBefore(div, heading);
-
-  setTimeout(clearError, 3000);
-}
-function clearError() {
-  document.querySelector(".alert").remove();
+  e.preventDefault();
 }
