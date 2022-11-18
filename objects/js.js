21d7a201892f65b2d62dbf05e36722cf6946b4d2
Erlan Abdullaev <abdullaev.erlan.kk@gmail.com>
2022-11-18 23:19:20 +0600

    add js change

--- objects/js.js
+++ objects/js.js
@@ -1,8 +1,21 @@
-const john = {
-  name: "john",
-  age: 18,
-  sayName: function () {
-    console.log(this.name);
-  },
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+//   console.log(this);
+// }
+// const john = new Person("john", 20);
+
+//======
+function Person(name, birthday) {
+  this.name = name;
+  this.birthday = new Date(birthday);
+}
+Person.prototype.calculateAge = function () {
+  const diff = Date.now() - this.birthday.getTime();
+  const ageDate = new Date(diff);
+  return ageDate.getUTCFullYear() - 1970;
 };
-john.sayName();
+const john = new Person("john", "10-10-1980");
+const mary = new Person("mary", "10-10-1999");
+console.log(john.calculateAge());
+console.log(mary.calculateAge());
